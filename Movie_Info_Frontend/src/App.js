ff65a0f248eee291be1a540d3d3b96f556c3637c
Roopam Joshi <roopamjoshi63@gmail.com>
2021-11-17 22:21:52 +0530

    basic login ui

---
+++ Movie_Info_Frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./component/forms/login";
+import Registration from "./component/forms/reg";
+
+function App() {
+  return (
+    <div>
+      <Registration />
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
