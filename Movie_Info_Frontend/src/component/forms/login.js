c537cfc83501ac87935a3e0941623dd521fda260
Roopam Joshi <roopamjoshi63@gmail.com>
2021-11-18 22:55:46 +0530

    login validation done and some css cahnge

--- Movie_Info_Frontend/src/component/forms/login.js
+++ Movie_Info_Frontend/src/component/forms/login.js
@@ -1,44 +1,87 @@
+import { useState } from "react";
 import React from "react";
+import Joi from "joi";
 import "./login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [validationError, setValidationError] = useState("");
+
+  const userSchema = Joi.object({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required()
+      .min(4)
+      .lowercase()
+      .label("E-Mail"),
+    pass: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required()
+      .min(6)
+      .label("Password"),
+  });
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    const result = userSchema.validate({ email: email, pass: password });
+    console.log(result);
+    console.log(result.error);
+
+    console.log(validationError.email);
+    if (result.error) {
+      setValidationError(result.error);
+    }
+  };
   return (
     <div>
       <div className="main">
         <div className="container">
           <div className="signup-content">
-            <form method="POST" id="signup-form" class="signup-form">
+            <form
+              method="POST"
+              id="signup-form"
+              className="signup-form"
+              onSubmit={handleSubmitForm}
+            >
               <h2>Login</h2>
               <div className="form-group">
                 <input
-                  type="username"
+                  type="email"
                   className="form-input"
-                  name="username"
-                  id="username"
-                  placeholder="Username"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
                   autoComplete="off"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                 />
-                <small></small>
               </div>
               <div className="form-group">
                 <input
-                  type="text"
+                  type="password"
                   className="form-input"
                   name="password"
                   id="password"
                   placeholder="Password"
                   autoComplete="off"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
                 />
-                <small></small>
+              </div>
+              <div className="message">
+                {validationError.message}
               </div>
               <button className="submit-form">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              Submit
-            </button>
-            <span className="existing">Not a member<a href="/login">sign up</a></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+              </button>
+              <span className="existing">
+                Not a member<a href="/reg">sign up</a>
+              </span>
             </form>
           </div>
         </div>
