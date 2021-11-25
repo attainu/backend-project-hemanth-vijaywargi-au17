f6c0de97120df7068aa8d79da6cf4d485afcb760
Hemanth Vijaywargi <hemanthvj12@gmail.com>
2021-11-25 20:54:32 +0530

    progress

--- Movie_Info_Server/server.js
+++ Movie_Info_Server/server.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const morgan = require("morgan");
+const { resolve } = require("path");
 
 // Server
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
 // MiddleWares
 app.use(fileUpload());
@@ -14,6 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
+
+// Statically Serving Build Folder
+const buildFolderPath = resolve(__dirname, "../Movie_Info_Frontend/build");
+app.use(express.static(buildFolderPath));
 
 // Database
 require("./db.js").dbConnect();
@@ -26,7 +31,12 @@ app.use("/movie", movieRoutes);
 const actorRoutes = require("./routes/actorRoutes.js");
 app.use("/actor", actorRoutes);
 
+//React Router
+app.get("*", (req, res) => {
+  res.sendFile(`${buildFolderPath}/index.html`);
+});
+
 // Starting the Server
-app.listen(port, () => {
-  console.log(`app listening on port : ${port}`);
+app.listen(PORT, () => {
+  console.log(`app listening on port : ${PORT}`);
 });
