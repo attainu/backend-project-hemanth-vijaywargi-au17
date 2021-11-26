757c2ca87652479fbb710a83f3dddfb0e0e5a46a
Hemanth Vijaywargi <hemanthvj12@gmail.com>
2021-11-26 23:23:07 +0530

    fixed the redux devtools bug

--- Movie_Info_Frontend/src/Store.js
+++ Movie_Info_Frontend/src/Store.js
@@ -4,9 +4,10 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./Reducers";
 
+const isProduction = process.env.NODE_ENV || 'production'
 const composedEnhancer = compose(
   applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  (isProduction !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose
 );
 
 // Creating our Global State/Store
